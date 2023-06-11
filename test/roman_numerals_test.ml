bf242e2e1180aa7a69b144ebb77871c3f48d7965
Ivan Blazhko <iblazhko@fastmail.in>
2023-06-11 11:15:33 +0100

    (Red) 2 translates to II

--- test/roman_numerals_test.ml
+++ test/roman_numerals_test.ml
@@ -3,11 +3,17 @@ module SUT = struct
 end
 
 (* Tests *)
+
+(* single literals *)
 let test_literal_I () =
   Alcotest.(check string) "1 translates to I" "I" (SUT.romanize 1)
 
 let test_literal_V () =
   Alcotest.(check string) "5 translates to V" "V" (SUT.romanize 5)
+
+(* repeating literals *)
+let test_repeating_literals_II () =
+  Alcotest.(check string) "2 translates to II" "II" (SUT.romanize 2)
 
 (* Tests Runner *)
 let () =
@@ -19,4 +25,6 @@ let () =
           test_case "1 -> I" `Quick test_literal_I;
           test_case "5 -> V" `Quick test_literal_V;
         ] );
+      ( "Repeating literals",
+        [ test_case "2 -> II" `Quick test_repeating_literals_II ] );
     ]
